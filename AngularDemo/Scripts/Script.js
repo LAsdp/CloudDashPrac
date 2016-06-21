e5f734de84bdb0a7cbadd7c90a9c7d203e449175
amsok <amsok@DESKTOP-J5MMAR9>
2016-06-21 15:25:06 +1200

    First Commit

---
+++ AngularDemo/Scripts/Script.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="angular.min.js" />
+
+angular.module("photoBrowser", [])
+    .service("imageService", function ($http) {
+        return {
+            getImages: function () {
+                return $http.get("/photos.json", { responseType: "json" });
+            }
+        }
+    })
+    .controller("photoList", function ($scope, imageService) {
+        imageService.getImages().success(function (result) {
+            $scope.images = result;
+        })
+
+  
+    })
